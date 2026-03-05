fbfd9ef1841ea44c3915c2d4c52e25c036bc125b
bahaelaila7 <bahaelaila7@gmail.com>
2026-03-05 14:20:36 -0500

    Create 7_DATA_COHORTS_DSTRB.sql

---
+++ 7_DATA_COHORTS_DSTRB.sql
@@ -0,0 +1,15 @@
+-- GROUP into cohorts of subplot, ecoregion, species, age. Calculate biomass
+-- FIA reports biomass as lbs/acre, LANDIS wants g/m2
+-- drybio_ag * TPA_UNADJ is in lbs/acre , so multiply by 0.112085 to become g/m^2 
+-- 1 lbs/acre * (1000/2.20462) g/lbs*   (1/4840) (acre/sq yard) * (1/0.9144)^2 (yard / m)^2  = 0.112085
+
+DROP TABLE IF EXISTS DATA_COHORTS_DSTRB;
+CREATE TABLE DATA_COHORTS_DSTRB AS
+SELECT v.PLT_CN, T.STATECD, T.UNITCD, T.COUNTYCD, T.PLOT,T.SUBP, v.measdate, T.SPECIES_SYMBOL, T.SPGRPCD, T.subp_has_dstrb,
+CAST ( round(v.age_calc) AS INTEGER) age_calc, sum(v.drybio_ag*v.TPA_UNADJ)*0.112085 AGB, sum(v.TPA_UNADJ) tree_count
+FROM DATA_SUBPLOTS_TREES_AGES_CALC T
+CROSS JOIN LATERAL unnest(T.PLT_CNs, T.drybio_ags, T.statuscds, T.ages_calc, T.measdates, T.TPA_UNADJs)
+v(PLT_CN, drybio_ag,statuscd, age_calc, measdate, TPA_UNADJ)
+WHERE v.statuscd = 1 and v.age_calc is not null and v.drybio_ag is not null and v.TPA_UNADJ > 0.0
+GROUP BY v.PLT_CN, T.STATECD, T.UNITCD, T.COUNTYCD, T.PLOT,T.SUBP, v.measdate, T.SPECIES_SYMBOL, T.SPGRPCD, T.SFTWD_HRDWD, T.subp_has_dstrb, age_calc
+HAVING COALESCE(sum(v.drybio_ag*v.TPA_UNADJ)*0.112085 ,0)  > 0
