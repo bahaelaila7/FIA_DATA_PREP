3701ec7d017ca51b609a45fff7275dcbc3f33e7f
bahaelaila7 <bahaelaila7@gmail.com>
2026-03-21 21:04:45 -0400

    Update 7_DATA_COHORTS_DSTRB.sql
    You do not group by PLT_CN, dummy

--- 7_DATA_COHORTS_DSTRB.sql
+++ 7_DATA_COHORTS_DSTRB.sql
@@ -16,14 +16,13 @@ WHERE v.statuscd = 1 and v.age_calc is not null and v.drybio_ag is not null and 
 GROUP BY v.PLT_CN, T.STATECD, T.UNITCD, T.COUNTYCD, T.PLOT,T.SUBP, v.measdate, T.SPECIES_SYMBOL, T.SPGRPCD, T.SFTWD_HRDWD, T.subp_has_dstrb, age_calc),
 
 Measurements AS (
-SELECT PLT_CN, STATECD, UNITCD, COUNTYCD, PLOT, min(measdate) first_measdate, count(distinct measdate) plot_meas_num
+SELECT STATECD, UNITCD, COUNTYCD, PLOT, min(measdate) first_measdate, count(distinct measdate) plot_meas_num
 FROM Grouped_COHORTS
-GROUP BY PLT_CN, STATECD, UNITCD, COUNTYCD, PLOT)
+GROUP BY STATECD, UNITCD, COUNTYCD, PLOT)
 
 SELECT G.*, m.first_measdate, m.plot_meas_num, (G.measdate - m.first_measdate) / 365.25 SIM_YEAR
 FROM GROUPED_COHORTS G
 JOIN Measurements m on 
-G.PLT_CN=m.PLT_CN AND
 G.STATECD=m.STATECD AND
 G.UNITCD=m.UNITCD AND
 G.COUNTYCD=m.COUNTYCD AND
