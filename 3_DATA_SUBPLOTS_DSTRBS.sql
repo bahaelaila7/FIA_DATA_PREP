457c76a15a27d987a0eb65ddba6ed3e5fd53d510
bahaelaila7 <bahaelaila7@gmail.com>
2026-03-22 15:48:46 -0400

    Update 3_DATA_SUBPLOTS_DSTRBS.sql
    Needed twice grouping because subp may have multiple condids per measurement

--- 3_DATA_SUBPLOTS_DSTRBS.sql
+++ 3_DATA_SUBPLOTS_DSTRBS.sql
@@ -3,14 +3,21 @@ DROP TABLE IF EXISTS DATA_SUBPLOTS_DSTRBS;
 CREATE TABLE DATA_SUBPLOTS_DSTRBS AS
 SELECT DSD.*, (TRUE = ANY(DSD.DSTRBS)) subp_has_dstrb, DSD.measdates[array_position(DSD.DSTRBS,TRUE)] first_dstrb_measdate
 FROM (
-SELECT DS.*, array_agg(
+
+
+
+SELECT 
+DSCOND.STATECD, DSCOND.UNITCD, DSCOND.COUNTYCD, DSCOND.PLOT , DSCOND.SUBP, DSCOND.plt_cns, DSCOND.invyrs, DSCOND.measdates, DSCOND.subp_num_meas,
+ array_agg( DSCOND.DSTRBs ORDER BY DSCOND.invyr) DSTRBs
+FROM (
+SELECT DS.*, v.plt_cn, v.invyr, bool_or(
 		(Coalesce(cond.DSTRBCD1,0) != 0 AND coalesce(cond.DSTRBYR1,DATE_PART('year', DS.measdates[1])) >= DATE_PART('year', DS.measdates[1])) OR 
 		(Coalesce(cond.DSTRBCD2,0) != 0 AND coalesce(cond.DSTRBYR2,DATE_PART('year', DS.measdates[1])) >= DATE_PART('year', DS.measdates[1])) OR
 		(Coalesce(cond.DSTRBCD3,0) != 0 AND coalesce(cond.DSTRBYR3,DATE_PART('year', DS.measdates[1])) >= DATE_PART('year', DS.measdates[1])) OR
 		(Coalesce(cond.TRTCD1,0) = 10 AND coalesce(cond.TRTYR1,DATE_PART('year', DS.measdates[1])) >= DATE_PART('year', DS.measdates[1]))OR
 		(Coalesce(cond.TRTCD2,0) = 10 AND coalesce(cond.TRTYR2,DATE_PART('year', DS.measdates[1])) >= DATE_PART('year', DS.measdates[1]))OR
 		(Coalesce(cond.TRTCD3,0) = 10 AND coalesce(cond.TRTYR3,DATE_PART('year', DS.measdates[1])) >= DATE_PART('year', DS.measdates[1]))
-	 ORDER BY v.invyr) DSTRBs
+	 ) DSTRBs
 FROM DATA_SUBPLOTS DS
 CROSS JOIN LATERAL unnest(DS.PLT_CNS, DS.INVYRS) v(plt_cn,invyr) 
 JOIN SUBP_COND SUBCOND ON 
@@ -19,4 +26,7 @@ JOIN SUBP_COND SUBCOND ON
 JOIN COND ON 
 	v.plt_cn = COND.PLT_CN 
 	AND SUBCOND.CONDID = COND.CONDID
-GROUP BY DS.STATECD, DS.UNITCD, DS.COUNTYCD, DS.PLOT , DS.SUBP, DS.plt_cns, DS.invyrs, DS.measdates, DS.subp_num_meas) AS DSD;
+
+GROUP BY DS.STATECD, DS.UNITCD, DS.COUNTYCD, DS.PLOT , DS.SUBP, DS.plt_cns, DS.invyrs, DS.measdates, DS.subp_num_meas, v.plt_cn, v.invyr) AS DSCOND
+
+GROUP BY DSCOND.STATECD, DSCOND.UNITCD, DSCOND.COUNTYCD, DSCOND.PLOT , DSCOND.SUBP, DSCOND.plt_cns, DSCOND.invyrs, DSCOND.measdates, DSCOND.subp_num_meas) AS DSD;
